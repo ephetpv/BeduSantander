943483ab880a17f6f5396bf40c53e63cf1e821e7
ephetpv <71673693+ephetpv@users.noreply.github.com>
2020-11-19 19:42:48 -0600

    Update sesion_uno.sql

--- clase_1/sesion_uno.sql
+++ clase_1/sesion_uno.sql
@@ -31,36 +31,45 @@ double: De tipo numérico, números con decimales (más precisión que float)
 timestamp: De tipo fecha, combinación de fecha y hora
 */
 
-SELECT nombre FROM empleado;
+SELECT nombre 
+FROM empleado;
 /* Muestra el campo llamado nombre de la tabla empleado
    SELECT <campo> FROM <tabla> */
-SELECT * FROM empleado WHERE id_puesto >= 100 
+SELECT * FROM empleado 
+WHERE id_puesto >= 100 
 	AND id_puesto <= 200;
-  /* Para filtrar los resultados se utiliza la intrucción
-     WHERE seguido de una sentencia lógica. El asterisco se
-     utiliza seleccionar todos los campos.*/
+/* Para filtrar los resultados se utiliza la intrucción
+	WHERE seguido de una sentencia lógica. El asterisco se
+	utiliza seleccionar todos los campos.*/
   
 /* Reto 2
-Usando la base de datos tienda, escribe consultas que permitan
-responder las siguientes preguntas.
-¿Cuál es el nombre de los empleados con el puesto 4?
-¿Qué puestos tienen un salario mayor a $10,000?
-¿Qué articulos tienen un precio mayor a $1,000 y un iva mayor a 100?
-¿Qué ventas incluyen los artículo 135 o 963 y fueron hechas por los 
-empleados 835 o 369?*/
+Usando la base de datos tienda, escribe consultas que permitan responder las siguientes preguntas.*/
 
-SELECT nombre FROM empleado WHERE id_empleado=4;
-/* Nombre de empleado: Cassey*/
-SELECT id_puesto FROM puesto WHERE salario>10000;
-SELECT id_articulo, nombre FROM articulo WHERE precio>1000 
+# ¿Cuál es el nombre de los empleados con el puesto 4?
+SELECT nombre 
+FROM empleado 
+WHERE id_empleado=4;
+# ¿Qué puestos tienen un salario mayor a $10,000?
+SELECT * 
+FROM puesto 
+WHERE salario>10000;
+# ¿Qué articulos tienen un precio mayor a $1,000 y un iva mayor a 100?
+SELECT id_articulo, nombre 
+FROM articulo 
+WHERE precio>1000 
 	AND iva>100;
-SELECT id_venta FROM venta WHERE id_articulo IN (135,963) 
+# ¿Qué ventas incluyen los artículo 135 o 963 y fueron hechas por los empleados 835 o 369?
+SELECT id_venta, clave
+FROM venta 
+WHERE id_articulo IN (135,963) 
 	AND id_empleado IN (835,369);
 /* Es útil usar describe para saber el nombre de los campos 
 y SELECT * FROM <tabla> para visualizar la tabla*/
 
-SELECT * FROM puesto ORDER BY salario DESC LIMIT 5,10;
-/* El ordenamiento es ascendente por lo que puede omitirse
+SELECT * FROM puesto 
+ORDER BY salario DESC 
+LIMIT 5,10;
+/* El ordenamiento es ascendente por default
 y LIMIT sirva para limitar cuantas filas regresará la consulta
 LIMIT indicador_nfila, numfilas*/
 
@@ -68,4 +77,7 @@ LIMIT indicador_nfila, numfilas*/
 Usando la base de datos tienda, escribe una consulta que 
 permita obtener el top 5 de puestos por salarios.*/
 
-SELECT id_puesto FROM puesto ORDER BY salario DESC LIMIT 5;
+SELECT id_puesto,salario 
+FROM puesto 
+ORDER BY salario DESC 
+LIMIT 5;
